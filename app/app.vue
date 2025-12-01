abcd1c6a01a1080a9967849bf0a07f0eb8c733df
dpt1305 <tungplatin@gmail.com>
2025-12-02 05:24:35 +0700

    feat: rout

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,13 @@
 <template>
   <header>
-    Header content
+    <p>Header content</p>
+    <ul>
+      <li><nuxt-link to="/about">About</nuxt-link></li>
+      <li><nuxt-link to="/inform">Inform</nuxt-link></li>
+      <li><NuxtLink to="/about">About 1</NuxtLink></li>
+      <li><NuxtLink to="/users/123">User</NuxtLink></li>
+    </ul>
   </header>
   <NuxtPage />
-  <footer>
-    Footer content
-  </footer>
+  <footer>Footer content</footer>
 </template>
-
---
+++ app/pages/users/[id].vue
@@ -0,0 +1,15 @@
+<template lang="html">
+    <div>
+        <p>User : {{ $route.params.id  }}</p>
+    </div>
+</template>
+<script>
+import { routeLocationKey } from 'vue-router';
+
+export default {
+    
+}
+</script>
+<style scoped lang="ts">
+
+</style>
