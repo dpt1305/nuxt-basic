38c7707db4628dcb98cf0584339dc13644eff329
dpt1305 <tungplatin@gmail.com>
2025-12-02 05:39:17 +0700

    feat: first component

---
+++ app/components/btn/DefaultButton.vue
@@ -0,0 +1,10 @@
+<template lang="html">
+  <button
+    class="p-2 px-4 text-red bg-blue-500 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300"
+  >
+    <p>Default Button</p>
+    <slot></slot>
+  </button>
+</template>
+<script setup></script>
+<style scoped lang="css"></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import DefaultButton from "~/components/btn/DefaultButton.vue";
+</script>
+
 <template>
-    <div>
-       <h1>Trang chu</h1> 
-    </div>
+  <div>
+    <h1>Trang chu</h1>
+    <DefaultButton />
+  </div>
 </template>
